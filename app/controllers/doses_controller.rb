8bedaa429f165f33820156f036af974ac71b2a98
Tom Niblo <lolcust@live.co.uk>
2019-11-08 11:01:31 +0900

    Added Heroku

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,36 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    id = params[:dose]
+    @dose.ingredient = Ingredient.find(id[:ingredient_id])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail[:id])
+    else
+      flash[:notice] = @dose.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+ingredients_json = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(ingredients_json)
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
